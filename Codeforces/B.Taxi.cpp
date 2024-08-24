cc4fbba29f728d4e67f189fd858aeaeec4256e57
Eduardo Zabala <jhonzabala329026@correo.itm.edu.co>
2024-08-24 09:51:43 -0500

    Correccion B-Taxi

--- Codeforces/B.Taxi.cpp
+++ Codeforces/B.Taxi.cpp
@@ -1,18 +1,54 @@
 #include <iostream>
 #include <algorithm>
-#include <cmath> 
+#include <vector>
 using namespace std;
-int main(){
-    int t,acum=0,num;
-    cin >> t ;
-    for(int i = 0; i < t; i++){
-        cin>>num;
-        acum+=num;
+int main()
+{
+    int t, x, taxis = 0;
+    cin >> t;
+    vector<int> people(4, 0);
+    for (int i = 0; i < t; i++)
+    {
+        cin >> x;
+        people[x - 1]++;
     }
-    if(acum%4!=0){
-      t= (round(acum/4) + 1 );  
+ 
+    // cout << "Antes\n";
+ 
+    // for_each(people.begin(),people.end(),[](int i){cout << i << " ";});
+    taxis = people[3];
+    people[3] = 0;
+    int min_group = min(people[2], people[0]);
+ 
+    people[0] -= min_group;
+    people[2] -= min_group;
+    taxis += min_group;
+    taxis += people[2];
+    people[2] = 0;
+ 
+    taxis += people[1] / 2;
+    people[1] %= 2;
+ 
+    if (people[1] != 0)
+    {
+        taxis++;
+        people[0] -= min(2,people[0]);
     }
-    t = acum/4;
-    cout << t; 
+    
+    if (people[0] != 0 && people[0] > 3)
+    {
+        taxis += people[0] / 4;
+        people[0] %= 4;
+        if (people[0] != 0)
+            taxis++;
+    }
+    else if (people[0] != 0)
+    {
+        taxis++;
+    }
+    people[0] = 0;
+ 
+    cout << taxis;
+ 
     return 0;
 }
